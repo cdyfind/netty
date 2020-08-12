a071b8860e956f2f0a5e5c11a0b89a6a648fb532
cao_d <cao_dong_yang@163.com>
2020-08-12 11:57:30 +0800

    字符串换行解析

--- src/main/java/com/java/netty/config/server/NettyServerHandlerInitializer.java
+++ src/main/java/com/java/netty/config/server/NettyServerHandlerInitializer.java
@@ -2,10 +2,12 @@ package com.java.netty.config.server;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
 
 public class NettyServerHandlerInitializer extends ChannelInitializer<Channel> {
     @Override
@@ -20,6 +22,9 @@ public class NettyServerHandlerInitializer extends ChannelInitializer<Channel> {
 //                .addLast(new ProtobufDecoder(MessageBase.Message.getDefaultInstance()))
 //                .addLast(new ProtobufVarint32LengthFieldPrepender())
 //                .addLast(new ProtobufEncoder())
+                //字符串解析，换行防拆包
+                .addLast(new LineBasedFrameDecoder(1024))
+                .addLast(new StringDecoder())
                 .addLast(new NettyServerHandler());
     }
 }
